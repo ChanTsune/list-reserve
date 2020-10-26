cccc854b9d76e90545673bcd4953bf91329f88d5
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-10-26 14:25:31 +0900

    Support type hint

---
+++ list_reserve/__init__.pyi
@@ -0,0 +1,13 @@
+from typing import Any, List
+
+
+def reserve(list: List[Any], capacity: int) -> None:
+    """Reserve list capacity."""
+
+
+def capacity(list: List[Any]) -> int:
+    """Return list capacity."""
+
+
+def shrink_to_fit(list: List[Any]) -> None:
+    """Shrink to fit list capacity."""
