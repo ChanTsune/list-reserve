ac78f3be07a22787e2d064b31b8d44baee257d8b
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-10-31 00:35:53 +0900

    Support type hint

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setup(name='list_reserve',
       description='Python builtin list memory allocation library',
       long_description=read('README.md'),
       long_description_content_type="text/markdown",
+      packages=['list_reserve'],
+      package_data={'list_reserve': ["py.typed", "__init__.pyi"]},
       ext_modules=[
         Extension('list_reserve', ['src/list_reserve.c'])],
       url='https://github.com/ChanTsune/list-reserve',
