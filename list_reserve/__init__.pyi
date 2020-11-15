13e9862a8d35776b7b3000a06a226e101e6e6be0
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-11-15 14:10:15 +0900

    Add type hint

--- list_reserve/__init__.pyi
+++ list_reserve/__init__.pyi
@@ -4,10 +4,11 @@ from typing import Any, List
 def reserve(list: List[Any], capacity: int) -> None:
     """Reserve list capacity."""
 
-
 def capacity(list: List[Any]) -> int:
     """Return list capacity."""
 
+def capacity_bytes(list: List[Any]) -> int:
+    """Return list allocated bytes."""
 
 def shrink_to_fit(list: List[Any]) -> None:
     """Shrink to fit list capacity."""
