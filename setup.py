12ee27db1e20e78301d6a36af596c9e3ef308d7e
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-12-15 14:55:15 +0900

    support python 3.10

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(name="list_reserve",
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
           'License :: OSI Approved :: MIT License',
       ],
       )
